6b9eba700c1ec85cc75e5477cbae1d59aabb3d38
Akshay Parseja <akshayparseja@gmail.com>
2019-07-03 17:10:57 +0530

    added-environ var-1

--- btre/settings.py
+++ btre/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = '0+%^0!o)e9vh&y^#kr3q6x*s-sjbr44zd224v+zo@ft*ujth^w'
-SECRET_KEY=os.environ.get('SECRET_KEY')# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY=os.environ.get('SECRET_KEY')
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['parseja-btre.herokuapp.com']
